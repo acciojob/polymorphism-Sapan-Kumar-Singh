357a22d13766e8223a2b72aef65cb36e23d0401f
Sapan-Kumar_Singh <spankumar006007@gmail.com>
2023-04-15 01:29:27 +0530

    First commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,24 @@ package com.driver;
 
 public class Main {
 
+    public class Product{
+
+        public int product(int x,int y){
+            return 0;
+        }
+        public int product(int x,int y,int z){
+            return 0;
+        }
+        public double product(double x,double y){
+             return 0.0;
+        }
+    }
+
+    public void main(String[] args){
+        Product p= new Product();
+        p.product(1,2);
+        p.product(1,2,3);
+        p.product(2.3,2.5);
+    }
+
 }
