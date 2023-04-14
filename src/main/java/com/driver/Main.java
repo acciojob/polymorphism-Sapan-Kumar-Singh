fbf5f97793f3318dc789584f064a2631c588e5fd
Sapan-Kumar_Singh <spankumar006007@gmail.com>
2023-04-15 01:46:21 +0530

    second commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,15 +4,9 @@ public class Main {
 
     public class Product{
 
-        public int product(int x,int y){
-            return 0;
-        }
-        public int product(int x,int y,int z){
-            return 0;
-        }
-        public double product(double x,double y){
-             return 0.0;
-        }
+        public int product(int x,int y){ return 0;}
+        public int product(int x,int y,int z){return 0;}
+        public double product(double x,double y){return 0.0;}
     }
 
     public void main(String[] args){
